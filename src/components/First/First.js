d4bdf95a92b3826e0307e63077eb0d99579791b8
mdjamilaj <mdjamilaj1@gmail.com>
2020-09-28 15:29:39 +0600

    props

--- src/components/First/First.js
+++ src/components/First/First.js
@@ -3,20 +3,21 @@ import './First.css';
 
 
 
-function First() {
+function First(props) {
+    console.log(props);
     return (
-        <div class="center">
-            <div class="property-card">
+        <div className="center">
+            <div className="property-card">
                 <a href="#">
-                    <div class="property-image">
-                        <div class="property-image-title">
-                            <h5>আমজাদ আলি</h5>
+                    <div className="property-image">
+                        <div className="property-image-title">
+                            <h5>{ props.res.name }</h5>
                         </div>
                     </div>
                 </a>
-                <div class="property-description">
-                    <h5> মাননীয় মেয়র </h5>
-                    <p>প্রথমেই আক্কেলপুর পৌরসভার সকল পৌরবাসীকে জানাই আন্তরিক শুভেচ্ছা। আমাদের প্রাতিষ্ঠানিক ওয়েবসাইট পরিদর্শন করার জন্য অসংখ্য ধন্যবাদ। আক্কেলপুর পৌরসভা এর ওয়েবসাইটের মাধ্যমে পরিচিত করিয়ে দেওয়ার পদক্ষেপ গ্রহণ করেছে যা বৈদ্যুতিক স্বয়.....</p>
+                <div className="property-description">
+                    <h5> { props.res.designation } </h5>
+                    <p className="description"> { props.res.description } </p>
                 </div>
             </div>
         </div>
